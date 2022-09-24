6e1adde3653f4c143a9a24a62279de7f30b3857c
Qing Li <dylan.liqing@gmail.com>
2022-09-24 10:13:02 +0800

    Update

--- syntax/__init__.py
+++ syntax/__init__.py
@@ -3,7 +3,7 @@ from .parser import Parser, PartialParse
 def build(config):
     n_tokens = len(config.domain.i2w)
     i2arity = [config.domain.sym2arity[config.domain.i2w[i]] for i in range(n_tokens)] \
-        if getattr(config.domain, 'enforce_arity_parsing', False) else None
+        if getattr(config.domain, 'enforce_arity_parsing', True) else None
     model = Parser(n_tokens, i2arity)
     return model
 
